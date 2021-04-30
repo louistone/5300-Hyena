ea16986804b54a0d731493712bd6b0b7a2f88f10
yinhui-evelyn <71414819+evelynliyh@users.noreply.github.com>
2021-04-30 00:38:49 -0700

    tried

--- SQLExec.cpp
+++ SQLExec.cpp
@@ -48,8 +48,8 @@ QueryResult::~QueryResult() {
 
 
 QueryResult *SQLExec::execute(const SQLStatement *statement) {
-    // FIXME: initialize _tables table, if not yet present
-
+    if (SQLExec::tables == nullptr)
+        SQLExec::tables = new Tables();
     try {
         switch (statement->type()) {
             case kStmtCreate:
@@ -71,24 +71,80 @@ SQLExec::column_definition(const ColumnDefinition *col, Identifier &column_name,
     throw SQLExecError("not implemented");  // FIXME
 }
 
+// create table
+// .py line 100
 QueryResult *SQLExec::create(const CreateStatement *statement) {
+
+    string table_name = statement->; //not sure what statement can point to
+    string columns = statement->;
+
+    // look at HeapTable.cpp 
+    // in storage_engine.h
+    // update _tables schema
+    ValueDict* t = new ValueDict();
+    Value v_tableName = Value(table_name);
+    (*t)[table_name] = v_tableName;
+    tables->insert(t);
+
+    try {
+        // update _columns schema
+        for(c: )
+        {
+
+        }
+        try {
+            for(column_name: Columns)
+
+        } catch() {
+
+        }
+
+
+    } catch(DbRelationError) {
+
+    }
+
     return new QueryResult("not implemented"); // FIXME
 }
 
+// drop table
 // DROP ...
 QueryResult *SQLExec::drop(const DropStatement *statement) {
+    if(statement->type != hsql::DropStatement::kTable)
+        throw SQLExecError("unrecognized DROP type");
+    
+    string table_name = statement->; 
+    if (table_name. TABLE_NAME)//in schema tables.h
+    {
+        throw SQLExecError("Cannot drop a schema table!");
+    }
+
+
+    delete handles;
+
+    table.drop();
+    SQLExec::tables->del(*SQLExec::tables->select(&where)->begin());
+
     return new QueryResult("not implemented"); // FIXME
 }
+
 
 QueryResult *SQLExec::show(const ShowStatement *statement) {
     return new QueryResult("not implemented"); // FIXME
 }
 
+// show tables
 QueryResult *SQLExec::show_tables() {
     return new QueryResult("not implemented"); // FIXME
 }
 
+// show columns
 QueryResult *SQLExec::show_columns(const ShowStatement *statement) {
+    // .py line 58
+    string table_name = statement->;
+    
+
+
     return new QueryResult("not implemented"); // FIXME
 }
 
